c4eef45132f706d6d0954186c6cbb1dde7b4f188
fedegiovannetti <giovannettipsi@gmail.com>
2025-04-28 19:48:50 -0300

    arreglando errores

--- script/request.py
+++ script/request.py
@@ -7,6 +7,9 @@ credenciales = {
     "client_secret": os.getenv("CLIENT_SECRET")
 }
 
+data_dir = os.path.join(os.getcwd(), 'data')  # Absolute path to 'data' folder
+os.makedirs(data_dir, exist_ok=True)
+
 link_api = 'https://apitransporte.buenosaires.gob.ar/ecobici/gbfs/stationStatus?'
 
 response = requests.get(link_api, params=credenciales)
@@ -25,4 +28,7 @@ estaciones_status_df["station_id"] = estaciones_status_df["station_id"].astype(i
 
 datos_filtrados = estaciones_status_df[(estaciones_status_df["station_id"] == 420) | (estaciones_status_df["station_id"] == 464)]
 
-datos_filtrados.to_csv('data/stations.csv', index=False)
+csv_file_path = os.path.join(data_dir, 'stations.csv')
+datos_filtrados.to_csv(csv_file_path, index=False)
+
+print(f"CSV saved to {csv_file_path}")
