0c7ee03152ec81410aeb2a8cba6391eefba734dd
fedegiovannetti <giovannettipsi@gmail.com>
2025-04-28 19:38:51 -0300

    Que guarde un csv con los datos

--- script/request.py
+++ script/request.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import pandas
 
 credenciales = {
     "client_id": os.getenv("CLIENT_ID"),
@@ -12,3 +13,16 @@ response = requests.get(link_api, params=credenciales)
 
 print(f"Status code: {response.status_code}")
 print(response.text)
+
+estaciones_status = response.json()
+
+estaciones_status_df = pd.DataFrame(estaciones_status["data"])
+estaciones_status_df = pd.json_normalize(estaciones_status_df['stations'])
+estaciones_status_df
+
+estaciones_status_df["station_id"] = estaciones_status_df["station_id"].astype(int) 
+
+
+datos_filtrados = estaciones_status_df[(estaciones_status_df["station_id"] == 420) | (estaciones_status_df["station_id"] == 464)]
+
+datos_filtrados.to_csv('data/stations.csv', index=False)
